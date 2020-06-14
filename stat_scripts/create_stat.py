94108740fc312f06fd31effecd03518ab0f87d91
yambulatof <44466084+yambulatof@users.noreply.github.com>
2020-06-14 18:13:37 +0300

    Delete spaces in .csv output

--- stat_scripts/create_stat.py
+++ stat_scripts/create_stat.py
@@ -8,6 +8,6 @@ output = subprocess.run(sys.argv[1:],stdout=subprocess.PIPE)
 sout = output.stdout.decode()
 p = sout.find('Statistics')
 ls = sout[p:].splitlines()[1:]
-print('Time, Steps, TimePerStep')
+print('Time,Steps,TimePerStep')
 for row in ls:
     print(row)
